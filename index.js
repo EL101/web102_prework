4aca70680c870124b89c7525023cc6d74c9b44fd
Ethan Liu <eliu3152@gmail.com>
2026-03-04 17:32:40 -0500

    Complete Challenge 4

--- index.js
+++ index.js
@@ -94,10 +94,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const unfunded = GAMES_JSON.filter(x => x.goal > x.pledged);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfunded);
+    return unfunded;
 }
 
 // show only games that are fully funded
@@ -105,10 +106,11 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const funded = GAMES_JSON.filter(x => x.goal <= x.pledged);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(funded);
+    return funded;
 }
 
 // show all games
@@ -116,7 +118,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -125,6 +127,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", e => filterUnfundedOnly());
+fundedBtn.addEventListener("click", e => filterFundedOnly());
+allBtn.addEventListener("click", e => showAllGames());
 
 
 /*************************************************************************************
